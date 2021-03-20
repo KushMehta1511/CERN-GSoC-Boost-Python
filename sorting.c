caea19c5199f1075e8ab8a4c603ab669f95e5ad9
KushMehta1511 <65716045+KushMehta1511@users.noreply.github.com>
2021-03-20 17:38:47 +0530

    Update sorting.c

--- sorting.c
+++ sorting.c
@@ -6,8 +6,9 @@ void sort(int *nums, int size){
   
     int *sortedNums = (int*)malloc(size*sizeof(int));
     // Create a count array to store count of individual characters and initialize count array as 0
-    //Let range be 10
-    int count[RANGE + 1], i;
+    //Let max element range be 10
+    const int max = 10; 
+    int count[max + 1], i;
     //initialising to 0
     memset(count, 0, sizeof(count));
  
@@ -16,7 +17,7 @@ void sort(int *nums, int size){
         count[nums[i]]++;
  
     // Change count[i] so that count[i] now contains actual position of this character in output array
-    for (i = 1; i <= RANGE; ++i)
+    for (i = 1; i <= max; ++i)
         count[i] += count[i - 1];
  
     // Using the count array fill the sortedNums array
